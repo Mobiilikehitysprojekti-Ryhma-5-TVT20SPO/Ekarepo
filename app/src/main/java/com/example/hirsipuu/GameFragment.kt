5db3763705a221c21c99eba5079e44b8d62d27ab
Teemu <t9jote00@students.oamk.fi>
2022-03-29 16:07:39 +0300

    Grafiikka kuva tehty

--- app/src/main/java/com/example/hirsipuu/GameFragment.kt
+++ app/src/main/java/com/example/hirsipuu/GameFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -13,8 +15,19 @@ class GameFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         val binding=inflater.inflate(R.layout.game_fragment, container, false)
-        lose()
+        val btn = binding.findViewById<Button>(R.id.guessBut)
+        var vaihda:Int = 1
+        val imageView = binding.findViewById<ImageView>(R.id.imageView2)
+        imageView.setImageResource(R.drawable.hirsipuu1)
+
+        btn.setOnClickListener( View.OnClickListener {
+            vaihda=vaihdakuva(vaihda,binding);
+        })
+
+
         return binding
     }
     fun victory(){
@@ -22,5 +35,22 @@ class GameFragment : Fragment() {
     }
     fun lose(){
         findNavController().navigate(R.id.action_gameFragment_to_loseFragment)
+    }
+    private fun vaihdakuva(vaihda:Int,binding:View):Int {
+        val imageView = binding.findViewById<ImageView>(R.id.imageView2)
+        val numero = vaihda+1
+        val kuva= when (numero) {
+            2-> R.drawable.hirsipuu2
+            3-> R.drawable.hirsipuu3
+            4-> R.drawable.hirsipuu4
+            5-> R.drawable.hirsipuu5
+            6-> R.drawable.hirsipuu6
+            7-> R.drawable.hirsipuu7
+            else-> R.drawable.hirsipuu8
+        }
+
+
+        imageView.setImageResource(kuva)
+        return numero
     }
 }
