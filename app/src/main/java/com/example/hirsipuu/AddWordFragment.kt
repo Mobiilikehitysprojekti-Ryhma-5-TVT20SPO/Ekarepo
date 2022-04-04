323b3bc0d72fe9dd5dbd6f2b96abeb68e086f80b
rami mustonen <t0mura00@students.oamk.fi>
2022-04-04 13:45:32 +0300

    pohjaa tehty sanojen lisäykseen

--- app/src/main/java/com/example/hirsipuu/AddWordFragment.kt
+++ app/src/main/java/com/example/hirsipuu/AddWordFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 
@@ -14,9 +16,28 @@ class AddWordFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = inflater.inflate(R.layout.add_word_fragment, container, false)
-        val backToStart : Button = binding.findViewById(R.id.backToMainBut)
-        backToStart.setOnClickListener{view:View->view.findNavController().navigate(R.id.action_addWordFragment_to_titleFragment)}
+        val binding = inflater.inflate(R.layout.add_word_fragment, container, false)    //Get fragment
+        val addText : EditText = binding.findViewById(R.id.addWordText)     //Get textinput
+        val addWordBut : Button = binding.findViewById(R.id.addWordBut)     //Get word add button
+        val backToStart : Button = binding.findViewById(R.id.backToMainBut) //Get back button
+        addWordBut.setOnClickListener{
+            val text = addText.text.toString().lowercase()                                      //Get string from textinput
+            if (text.isNotEmpty()){                                                             //Check that text is not empty
+                if(text.contains("[0-9!\"#$%&'()*+,-./:;\\\\<=>?@\\[\\]^_`{|}~]".toRegex())) {  //Check if word contains special characters or numbers
+                    Toast.makeText(context, "Word contains numbers or special characters", Toast.LENGTH_SHORT).show()   //Tell user why can't add word
+                }else{
+                    if (text.contains("ä") or text.contains("ö")) {                 //Check if word contains non english alphabets
+                        Toast.makeText(context, "Word contains non english letters", Toast.LENGTH_SHORT).show() //Tell user why can't add word
+                    } else {
+                        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else{
+                Toast.makeText(context, "Text empty", Toast.LENGTH_SHORT).show()    //Tell user why can't add word
+            }
+        }
+        backToStart.setOnClickListener{view:View->view.findNavController().navigate(R.id.action_addWordFragment_to_titleFragment)}  //Navigate to tittle fragment
         return binding
     }
 }
