ed6398fb742efa2c8595ef20652b74d1cecf1171
rami mustonen <t0mura00@students.oamk.fi>
2022-04-08 12:08:47 +0300

    Koodi kommentoitu

--- app/src/main/java/com/example/hirsipuu/WordListFragment.kt
+++ app/src/main/java/com/example/hirsipuu/WordListFragment.kt
@@ -16,22 +16,22 @@ class WordListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val wordarray = arrayOf("testing1","testing2")
-        val numarray = arrayOf(1,2)
+        val wordarray = arrayOf("testing1","testing2")      //Haetaan kaikki sanat
+        val numarray = arrayOf(1,2)                         //Haetaan sanoille yritysten määrä
         var text = ""
         var textnum = ""
-        val binding = inflater.inflate(R.layout.word_list_fragment, container, false)
-        val list : TextView = binding.findViewById(R.id.wordlisttext)
-        val listnum : TextView = binding.findViewById(R.id.wordlistnumtext)
+        val binding = inflater.inflate(R.layout.word_list_fragment, container, false)      //Otetaan View bindiin kiinni
+        val list : TextView = binding.findViewById(R.id.wordlisttext)                                 //Haetaan sana lista
+        val listnum : TextView = binding.findViewById(R.id.wordlistnumtext)                           //Haetaan yritys lista
         for (words in wordarray){
-            text += (words+System.getProperty("line.separator"))
+            text += (words+System.getProperty("line.separator"))                                      //Tehdään sana listasta yksi teksti rivin vaihdoilla
         }
         for (num in numarray){
-            textnum += (num.toString()+System.getProperty("line.separator"))
+            textnum += (num.toString()+System.getProperty("line.separator"))                          //Tehdään yritys listasta yksi teksti rivin vaihdoilla
         }
-        list.setText(text)
-        listnum.setText(textnum)
-        val backToStart : Button = binding.findViewById(R.id.backListBut)
+        list.setText(text)                                                                            //Asetetaan sana tekstit
+        listnum.setText(textnum)                                                                      //Asetetaan yritysten tekstit
+        val backToStart : Button = binding.findViewById(R.id.backListBut)                             //Kun nappia painetaan mennään title fragmenttiin
         backToStart.setOnClickListener{view:View->view.findNavController().navigate(R.id.action_wordListFragment_to_titleFragment)}
         return binding
     }
